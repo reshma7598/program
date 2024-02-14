bf26be3c56a1ecafdfa0329e7503a80681039fc6
reshma7598 <reshmarajanr136@gmail.com>
2024-02-14 10:40:53 +0530

    first commit

---
+++ large.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int main() {
+    // greatest number
+int numb1,numb2;
+    printf("Enter 2 numbers");
+    scanf("%d%d",&numb1,&numb2);
+    if(numb1>numb2){
+        printf("Greatest number is %d", numb1);
+    }else{
+        printf("Greatest number is %d", numb2);
+    }
+    return 0;
+    }
